5dc602e05f8809a991406e4234a9182776aab274
Rohan Subhash Darekar <darekarrohan3377@gmail.com>
2026-03-25 18:21:09 +0530

    Create OOP_Abstraction_code.py

---
+++ OOP_Abstraction_code.py
@@ -0,0 +1,12 @@
+class Calculator:
+    def Add(self,a,b):
+        return a + b
+    
+    def Subt(self,a,b):
+        return a-b
+        
+c = Calculator()
+print(c.Add(5,3))
+print(c.Subt(4,7))
+
+
