29303d34e924fed8a4ff1b5946f25f1258429050
Rohan Subhash Darekar <darekarrohan3377@gmail.com>
2026-03-25 19:53:22 +0530

    Create OOP_Polymorphism.py

---
+++ OOP_Polymorphism.py
@@ -0,0 +1,15 @@
+class dog:
+    def Sound(self):
+        print("ok dog ji")
+class Cat:      
+    def Sound(self):
+        print("ok cat ji")
+        
+Animal = [dog(), Cat()]
+
+for a in Animal:
+    a.Sound()
+    
+    
+        
+    
