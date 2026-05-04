c561c42320b6016075eb28a0417aaf508b7377f8
Rohan Subhash Darekar <darekarrohan3377@gmail.com>
2026-05-04 16:51:10 +0530

    Update (10) Reverse a string using loop.py

--- (10) Reverse a string using loop.py
+++ (10) Reverse a string using loop.py
@@ -1,12 +1,26 @@
-# 10 Reverse a string using loop (interview favourite)
-# suresh
-
-def reversed_string(s):
-    result = ""
-    
+# Method 1 (Using Built in functionality - Slice)
+def reverse_string(s):
+    s=s.lower()
+    result =""
     for ch in s:
-        if ch not in result:
-            result += ch
+         result += ch
+         
     return result[::-1]
-            
-print( reversed_string("suresh"))
+    
+print (reverse_string("Suresh"))
+
+# Method 2 (Without using built in functionality)
+
+def reverse_string(s):
+    s=list(s)
+    
+    left,right = 0 , len(s)-1
+    
+    s[left] , s[right] = s[right] ,s[left]
+    
+    left = left + 1
+    right = right -1
+    
+    return "".join(s)
+    
+print (reverse_string("oK"))
