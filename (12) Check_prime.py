da96a2833340ecb4956039acbee99577de1057f8
Rohan Subhash Darekar <darekarrohan3377@gmail.com>
2026-06-24 03:00:09 +0530

    Update (12) Check_prime.py

--- (12) Check_prime.py
+++ (12) Check_prime.py
@@ -1,12 +1,11 @@
-# 12. Check if number is prime
-
 def check_prime(s):
     if s <= 1:
-        return false
-    
-    for i in range(2,int(s*0.5)+1):
+        return False
+
+    for i in range(2, int(s ** 0.5) + 1):
         if s % i == 0:
             return False
-            break
+
     return True
+
 print(check_prime(25))
